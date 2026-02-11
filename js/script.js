22815fb05c37399695a15ff54b2e33f428d0e71a
Jatiro Feshago Panjalu <jatirofp@gmail.com>
2026-02-11 21:49:38 +0700

    Uploading from local to deploy first version online
    Uploading files to fulfill mini project requirements to get it online. By using GitHub Pages. This is my very first project.

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// Welcome dynamic name
+let name = "Jatiro";
+document.getElementById("welcomeText").innerText =
+    "Hi " + name + ", Welcome to Jetzkky Global Corporation";
+
+
+// Form Validation
+document.getElementById("messageForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let userName = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let message = document.getElementById("messageInput").value;
+
+    if (userName === "" || email === "" || phone === "" || message === "") {
+        alert("All fields must be filled!");
+        return;
+    }
+
+    if (!email.includes("@")) {
+        alert("Email must contain '@'");
+        return;
+    }
+
+    if (isNaN(phone)) {
+        alert("Phone must be numeric!");
+        return;
+    }
+
+    let resultText = `
+        Name: ${userName} <br>
+        Email: ${email} <br>
+        Phone: ${phone} <br>
+        Message: ${message}
+    `;
+
+    document.getElementById("result").innerHTML = resultText;
+});
